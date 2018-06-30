7662aa688e3cede8a4df0af05fe911a35510cca9
Juntao Hu <hujuntao@buaa.edu.cn>
2018-06-30 14:48:19 +0800

    import css from materialize, install axios

---
+++ src/components/OrganizationSpace/Main.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <header>
+      <nav id="top-nav" class="top-nav transparent">
+        <div class="container">
+          <div class="nav-wrapper">
+            <div class="row">
+              <div class="col s12 m10 offset-m1">
+                <h2 class="header black-text">会议管理</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div class="container">
+        <a href="#" data-target="nav-mobile" class="top-nav sidenav-trigger full hide-on-large-only">
+          <i class="material-icons">menu</i>
+        </a>
+      </div>
+      <ul id="slide-out" class="sidenav sidenav-fixed">
+        <li>
+          <div class="user-view">
+            <div class="background"
+                 style="background-image: url(https://materializecss.com/images/office.jpg); background-size: cover">
+              <!--<img src="https://materializecss.com/images/office.jpg">-->
+            </div>
+            <a href="#user"><img class="circle" src="https://materializecss.com/images/yuna.jpg"></a>
+            <a href="#name"><span class="white-text name">John Doe</span></a>
+            <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
+          </div>
+        </li>
+        <li class="bold active">
+          <router-link to="/orgspace" class="waves-effect">会议管理</router-link>
+        </li>
+        <li class="bold">
+          <router-link to="/orgspace" class="waves-effect">稿件管理</router-link>
+        </li>
+        <li class="bold">
+          <router-link to="/orgspace" class="waves-effect">机构信息设置</router-link>
+        </li>
+        <li class="bold">
+          <router-link to="/orgspace" class="waves-effect">机构管理员设置</router-link>
+        </li>
+        <li class="bold">
+          <router-link to="/orgspace" class="waves-effect">账户设置</router-link>
+        </li>
+      </ul>
+    </header>
+  </div>
+</template>
+
+<script>
+  import navbar from '@/include/NavBar';
+
+  export default {
+    name: "Main",
+    components: { navbar },
+    created: function () {
+      document.addEventListener('DOMContentLoaded', function () {
+        let elems = document.querySelectorAll('#slide-out');
+        let instances = M.Sidenav.init(elems, options);
+      });
+    }
+  }
+</script>
+
+<style scoped>
+  /*@media only screen and (max-width: 992px) {*/
+    /*nav .nav-wrapper {*/
+      /*text-align: center;*/
+    /*}*/
+    /*header {*/
+      /*padding-left: 0;*/
+    /*}*/
+  /*}*/
+
+  /*header {*/
+    /*padding-left: 300px;*/
+  /*}*/
+
+  /*#top-nav {*/
+    /*height: 110px;*/
+  /*}*/
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import OrganizationSpace from '@/components/OrganizationSpace/Main';
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/orgspace',
+      name: 'OrganizationSpace',
+      component: OrganizationSpace
     }
   ]
 })
