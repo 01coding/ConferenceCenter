7d23131d83a7a3dc6bb42156d6e98df0af921635
wuxingzhe <1479792108@qq.com>
2018-07-01 01:22:33 +0800

    add the foot

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
     <navbar></navbar>
-    <div style="height:30px"></div>
+    <div style="height:20px"></div>
     <div class="carousel carousel-slider">
       <a class="carousel-item"><img src="../../static/Image/W1.png"></a>
       <a class="carousel-item"><img src="../../static/Image/W2.png"></a>
       <a class="carousel-item"><img src="../../static/Image/W3.png"></a>
     </div>
+    <div style="height:10px;"></div>
+
+
+      <footer class="page-footer grey lighten-1" >
+        <div class="container" style="width:100%;height:100%;">
+          <div class="row" style="width:100%;">
+            <div class="row" style="width:100%;">
+              <span class="grey-text text-darken-4" style="position: absolute;left: 10px;">Association for the Advancement of Conferences </span>
+            </div>
+            <div class="row" style="width:100%;">
+              <span class="grey-text text-darken-4" style="position: absolute;left: 10px;">Telephone: 18811526200   Email; 1479792108@qq.com</span>
+            </div>
+            <div class="row" style="width:100%;">
+              <span class="grey-text text-darken-4" style="position: absolute;left: 10px;">Beihang University, Xueyuan Road   Haidian district, Beijing China</span>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="footer-copyright grey" style="width:100%;position: absolute;left: 0px;margin:0px auto;">
+          <span class="grey-text text-darken-4" style="position: absolute;left: 10px;">© 2018 Copyright.This site is protected by copyright and trademark laws under Chinese and International law. All rights reserved.</span>
+        </div>
+      </footer>
   </div>
 </template>
 
@@ -55,6 +78,7 @@ export default{
           fullWidth: true,
           indicators: true,
           duration: 100,
+          shift:30,
         }
       );
     });
