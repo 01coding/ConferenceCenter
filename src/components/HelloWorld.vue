14257545cf8ecd0b0a8fe919c7b3c26b2260142c
shmily <22733925535@qq.com>
2018-06-30 15:10:41 +0800

    add a page displayed a coference

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,8 +3,12 @@
   <!--<div class="ui fluid container">-->
     <navbar></navbar>
     <div class="hello">
-      <pagination v-bind:number="15" v-bind:current="1"></pagination>
-      <!--<ul class="pagination">-->
+      <!--v-if 用于判断该组件是否显示->
+
+      <pagination v-bind:number="15" v-bind:current="1" v-if="value>10"></pagination>
+
+      <route-link to="/conference"><botton>tijiao</botton></route-link>
+      <!--<ul clas s="pagination">-->
         <!--<li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>-->
         <!--<li class="active"><a href="#!">1</a></li>-->
         <!--<li class="waves-effect"><a href="#!">2</a></li>-->
@@ -26,8 +30,22 @@
     components: { navbar, pagination },
     data() {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        msg: 'Welcome to Your Vue.js App',
+        value:10,
+        href_str:"/set/fgs"
       }
+    },
+    methods(){
+      function setName(){
+        this.value=20;
+      }
+    },
+    created(){
+      this.setName();
+
+    },
+    mounted(){
+
     }
   }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import ConferenceInfo from '@/components/ConferenceInformation/Conference'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/conference',
+      name: 'ConferenceInfo',
+      component: ConferenceInfo
     }
   ]
 })
