b206c0952a8e4b34a3dd9aaab99feb523b7fd16a
buaa_luo <17801016258@163.com>
2018-07-02 00:28:28 +0800

    添加跨域支持

--- src/main.js
+++ src/main.js
@@ -8,19 +8,23 @@ import router from './router'
 Vue.config.productionTip = false;
 
 Vue.prototype.$axios = axios.create({
-  baseURL: 'http://118.89.229.204:8080/server-0.0.1-SNAPSHOT'
+  baseURL: 'http://118.89.229.204:8080/server-0.0.1-SNAPSHOT',
+  withCredentials: true // 允许携带cookie
 });
 
 Vue.prototype.$user = axios.create({
-  baseURL: 'http://118.89.229.204:3000'
+  baseURL: 'http://118.89.229.204:3000',
+  withCredentials: true // 允许携带cookie
 });
 
 Vue.prototype.$file = axios.create({
-  baseURL: 'http://140.143.19.133:8001'
+  baseURL: 'http://140.143.19.133:8001',
+  withCredentials: true // 允许携带cookie
 });
 
 Vue.prototype.$test = axios.create({
-  baseURL: 'http://123.206.54.85:3000/mock/14'
+  baseURL: 'http://123.206.54.85:3000/mock/14',
+  withCredentials: true // 允许携带cookie
 });
 
 /* eslint-disable no-new */
