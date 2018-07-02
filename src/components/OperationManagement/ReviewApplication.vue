c06d5645d749ab4babf1c54d2ce1af31729fd307
wuxingzhe <1479792108@qq.com>
2018-07-02 09:30:30 +0800

    add the background to the manage page

--- src/components/OperationManagement/ReviewApplication.vue
+++ src/components/OperationManagement/ReviewApplication.vue
@@ -1,24 +1,25 @@
 <template>
   <div>
+    <NavBar></NavBar>
+    <BackGround></BackGround>
     <div class="row">
       <div class="col s1"></div>
       <div class="col s10">
-    <h2>待审核申请</h2>
-    <ul class="collapsible" id="collaps">
-      <li v-for="(item, index) in apply" v-bind:key="index">
-        <div class="collapsible-header active"><i class="material-icons">filter_drama</i>{{item.name}}</div>
-        <div class="collapsible-body">
-          <p>单位地点：{{item.location}}</p>
-          <div class="center">
-            <img src="item.backimg"/>
-          </div>
-          <p>单位介绍：{{item.introduction}}</p>
-          <p>证明材料：<a href="item.evidence">点我获取</a></p>
-          <button class="btn" @click="reviewPass(item.id)">通过</button>
-          <button class="btn" @click="reviewReject(item.id)">拒绝</button>
-        </div>
-      </li>
-    </ul>
+        <ul class="collapsible" id="collaps">
+          <li v-for="(item, index) in apply" v-bind:key="index">
+            <div class="collapsible-header active"><i class="material-icons">filter_drama</i>{{item.name}}</div>
+            <div class="collapsible-body">
+              <p>单位地点：{{item.location}}</p>
+              <div class="center">
+                <img src="item.backimg"/>
+              </div>
+              <p>单位介绍：{{item.introduction}}</p>
+              <p>证明材料：<a href="item.evidence">点我获取</a></p>
+              <button class="btn" @click="reviewPass(item.id)">通过</button>
+              <button class="btn" @click="reviewReject(item.id)">拒绝</button>
+            </div>
+          </li>
+        </ul>
       </div>
       <div class="col s1"></div>
     </div>
@@ -27,9 +28,11 @@
 
 <script>
   import NavBar from "@/include/NavBar";
+  import BackGround from "@/include/Background";
+
   export default {
     name:"ReviewApplication",
-    components:{NavBar},
+    components:{NavBar, BackGround},
     data: function() {
       return {
         applyNumber: 0,
