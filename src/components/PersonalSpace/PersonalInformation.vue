a4a183a53e0f826e7c374da763bbb2e3f5943e68
wuxingzhe <1479792108@qq.com>
2018-07-05 02:15:19 +0800

    add a vue to the conference page

---
+++ src/components/PersonalSpace/PersonalInformation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default{
+  name:"PersonalInformation",
+  components:{},
+  data: function () {
+    return{
+
+    }
+  },
+  methods:{
+    created(){
+
+    }
+  }
+};
+</script>
