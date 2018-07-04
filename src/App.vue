6ab23f92b4bff51f4bd90b55542b8c392507e39a
Juntao Hu <hujuntao@buaa.edu.cn>
2018-07-04 18:44:13 +0800

    update review, modify scrollbar style

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
 <style>
   body {
     background: #f5f5f7;
-  },
+  }
   #app {
     /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
     font-family: 'Microsoft YaHei UI Light', Helvetica, Arial, sans-serif;
@@ -24,9 +24,33 @@
     /*text-align: center;*/
     color: #2c3e50;
     /*margin-top: 60px;*/
+    /*overflow: 'overlay';*/
   }
 
   pre {
     font-family: 'Microsoft YaHei UI Light', Helvetica, Arial, sans-serif;
+  }
+
+  ::-webkit-scrollbar {
+    width: 2px;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    /*box-shadow: inset 0 0 5px grey;*/
+    background: #fafafa;
+    /*background: white;*/
+    border-radius: 2px;
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: #009688;
+    border-radius: 2px;
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #009688;
   }
 </style>
