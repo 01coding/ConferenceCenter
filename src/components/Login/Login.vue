c41d7d93632f6d0a44039dba2d871478143510b5
willshaw <575386879@qq.com>
2018-07-05 21:27:18 +0800

    xwq

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -2,7 +2,7 @@
   <div>
     <navbar></navbar>
     <test-robot v-on:child-say="fill_form"></test-robot>
-    <div class="row" style="background: url(/static/bg7.jpg) no-repeat; background-size: cover;height: 658px;margin-bottom: 0;">
+    <div class="row" style="background: url(/static/bg7.jpg) no-repeat; background-size: cover;height:41.1em;margin-bottom: 0;">
       <div class="col s12 m4 offset-m4 l4 offset-l4" style="padding-top: 80px;width:35%">
         <div class="card-panel" style="padding-bottom: 5px;padding-top: 12px;opacity: 0.9">
           <div class="row center-align" style="margin-top:0; margin-bottom: 1.5rem;">
