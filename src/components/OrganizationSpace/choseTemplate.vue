7e6526b3dc5e5b07595cbbf00ce5549f0e614470
buaa_luo <17801016258@163.com>
2018-07-05 21:29:39 +0800

    添加悬浮样式选择项

--- src/components/OrganizationSpace/choseTemplate.vue
+++ src/components/OrganizationSpace/choseTemplate.vue
@@ -1,46 +1,45 @@
 <template>
   <div class="choseTP" v-if="ifShow">
+    <div class="bg "></div>
+    <div class="pp">
     <div class="row">
-      <div class="col s12 m3">
+      <div class="col s12 m3 " @click="chose(1)">
         <div class="card">
-          <div class="card-image">
-            <img src="https://materializecss.com/images/sample-1.jpg">
+          <div class="card-image hoverable">
+            <img src="http://118.89.229.204:8080/ERM-WebIO-1.0/uploads/9429689731530795759771325578045.jpg">
             <span class="card-title">简约</span>
             <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
           </div>
-          <div class="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-          </div>
+
         </div>
       </div>
 
-      <div class="col s12 m3">
+      <div class="col s12 m3 " @click="chose(2)">
         <div class="card">
-          <div class="card-image">
-            <img src="https://materializecss.com/images/sample-1.jpg">
-            <span class="card-title">雅致</span>
-            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-          </div>
-          <div class="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col s12 m3">
-        <div class="card">
-          <div class="card-image">
-            <img src="https://materializecss.com/images/sample-1.jpg">
+          <div class="card-image hoverable">
+            <img src="http://118.89.229.204:8080/ERM-WebIO-1.0/uploads/3220737015307958410542120607142.jpg">
             <span class="card-title">科技</span>
             <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
           </div>
-          <div class="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+
+        </div>
+      </div>
+
+      <div class="col s12 m3 " @click="chose(3)">
+        <div class="card">
+          <div class="card-image hoverable">
+            <img src="http://118.89.229.204:8080/ERM-WebIO-1.0/uploads/14354229441530795892543288075811.png">
+            <span class="card-title">绚丽</span>
+            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
           </div>
+
         </div>
       </div>
     </div>
+    </div>
+
   </div>
+
 </template>
 
 <script>
@@ -68,6 +67,11 @@
           },
           offTP:function () {
             this.ifShow=false
+          },
+          chose:function (num) {
+
+            this.$bus.emit("TPChoseOver",num)
+            this.offTP()
           }
 
         }
@@ -75,10 +79,31 @@
 </script>
 
 <style scoped>
+  .bg{
+    margin-left: -80px;
+    display: block;
+    z-index: 2;
+    position: fixed;
+    top: 0px;
+    min-width: 100%;
+    height: 100%;
+    /*background-color: rgba(1,1,1,0.4);*/
+
+  }
+  .blur {
+    -webkit-filter: blur(10px); /* Chrome, Opera */
+    -moz-filter: blur(10px);
+    -ms-filter: blur(10px);
+    filter: blur(10px);
+  }
 .choseTP{
-  position: absolute;
-  margin-top: 20%;
+
 
 }
+  .pp{
+    z-index: 3;
+    position: fixed;
+    top:30%;
+  }
 
 </style>
