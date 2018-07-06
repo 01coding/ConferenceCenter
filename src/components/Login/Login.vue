832953b8e629d62b81b3cfd89150fa1ce4cc8cb6
willshaw <575386879@qq.com>
2018-07-06 20:20:22 +0800

    xwq

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -5,7 +5,7 @@
     <div class="row customize" style="background: url(/static/bg10.jpg);background-size: cover"
          :style="{'height': (page_height-60)+'px'}">
       <div class="col s12 m4 offset-m4 l4 offset-l4" style="padding-top: 6.5rem;width:35%">
-        <div class="card-panel" style="padding-bottom: 5px;padding-top: 12px;opacity: 0.95; min-width: 30rem;">
+        <div class="card-panel" style="padding-bottom: 6%;padding-top: 4.7%;opacity: 0.95; min-width: 30rem;">
           <div class="row center-align" style="margin-top:0; margin-bottom: 1.5rem;">
             <h3>登录</h3>
           </div>
