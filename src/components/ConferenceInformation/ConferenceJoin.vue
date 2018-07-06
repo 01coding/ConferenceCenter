3efcd280d83d180a88f95ec2f9c09f9c8cb90431
shmily <22733925535@qq.com>
2018-07-06 13:56:30 +0800

    update ConferenceJoin.vue

--- src/components/ConferenceInformation/ConferenceJoin.vue
+++ src/components/ConferenceInformation/ConferenceJoin.vue
@@ -101,7 +101,7 @@
               <div class="row valign-wrapper" style="margin-bottom: 0;padding-top: 3px;padding-bottom: 3px;">
                   <div class="col s2 valign-wrapper">
                     <i class="material-icons prefix">person</i>
-                    性别
+                    <span style="margin-left: 1rem;">性别</span>
                   </div>
                   <label class="col s2">
                     <input type="radio" value="男" v-model="participant_field.sex" />
@@ -113,7 +113,7 @@
                   </label>
                   <div class="col s3 valign-wrapper">
                     <i class="material-icons prefix">home</i>
-                    预定住宿
+                    <span style="margin-left: 1rem;">预定住宿</span>
                   </div>
                   <div class="switch col s3">
                     <label>
@@ -542,25 +542,6 @@
             return;
           }
         }
-        //check participants information
-        /*if(type === 1) {
-          if(this.participants.length != 1) {
-            M.toast({
-              html: "<span style='font-weight: bold;'>您只能本人参会</span>",
-              classes: 'yellow darken-2 rounded'
-            });
-            return;
-          }
-        }
-        else {
-          if(this.participants.length < 1) {
-            M.toast({
-              html: "<span style='font-weight: bold;'>您需要填写至少一位参会者信息</span>",
-              classes: 'yellow darken-2 rounded'
-            });
-            return;
-          }
-        }*/
         //set parameters to transmit
         let participant_str = JSON.stringify(this.participants);
         let file_url = files[0].response;
